cf2c65df30643bedf1780fd64807c7b07e657ceb
Florian Kreuk <fkreuk@cartermario.com>
2023-10-12 08:58:11 -0400

    updated the schema and seeds to work properly

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,23 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE departments (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
+  name VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_id INT,
-    title varchar(30),
+    title varchar(45),
     salary decimal,
-    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name varchar(30),
     last_name varchar(30),
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
-    manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+    manager_id INT
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,27 @@
-INSERT INTO department (department_name)
-VALUES  (Engineering),
-        (Finance),
-        (Legal),
-        (Sales);
+Use employees_db;
+
+INSERT INTO department (name)
+VALUES  ("Engineering"),
+        ("Finance"),
+        ("Legal"),
+        ("Sales");
 
 INSERT INTO role (title, department_id, salary)
-VALUES  (Sales Lead, 4, 100000),
-        (Salesperson, 4, 80000),
-        (Lead Engineer, 1, 150000),
-        (Software Engineer, 1, 120000),
-        (Account Manager, 2, 160000),
-        (Accountant, 2, 125000),
-        (Legal Team Lead, 3, 250000),
-        (Lawyer, 3, 190000);
+VALUES  ("Sales Lead", 4, 100000),
+        ("Salesperson", 4, 80000),
+        ("Lead Engineer", 1, 150000),
+        ("Software Engineer", 1, 120000),
+        ("Account Manager", 2, 160000),
+        ("Accountant", 2, 125000),
+        ("Legal Team Lead", 3, 250000),
+        ("Lawyer", 3, 190000);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  (John, Doe, 1, NULL),
-        (Mike, Chan, 2, 1),
-        (Ashley, Rodriguez, 3, null),
-        (Kevin, Tupik, 4, 3),
-        (Kunal, Singh, 5, null),
-        (Malia, Brown, 6, 5),
-        (Sarah, Lourd, 7, null),
-        (Tom, Allen, 8, 7);
+VALUES  ("John", "Doe", 1, NULL),
+        ("Mike", "Chan", 2, 1),
+        ("Ashley", "Rodriguez", 3, null),
+        ("Kevin", "Tupik", 4, 3),
+        ("Kunal", "Singh", 5, null),
+        ("Malia", "Brown", 6, 5),
+        ("Sarah", "Lourd", 7, null),
+        ("Tom", "Allen", 8, 7);
